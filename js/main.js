5eb1336f214aab2216da056b7c5763088d7fcc1c
Aaron Fischer <aaron.fischer@caxiam.com>
2018-06-13 12:25:42 -0400

    WIP, temporarily inject bootstrap.min.css and jquery from CDN until example build system is setup
    Add main.js for basic interaction with example and terminal, setup base layout in index.html changes

---
+++ js/main.js
@@ -0,0 +1,50 @@
+jQuery(document).ready(function($) {
+  var Omnigage;
+  var oTerminal;
+
+  // handle embedding the terminal code into DOM
+  $('#form-render').on('submit', function (e) {
+    e.preventDefault();
+    var script = document.createElement('script');
+    script.text = $('#terminalEmbedCode').val();
+    document.body.appendChild(script);
+
+    // unlock/show step2
+    $('#remaining-steps').addClass('active');
+    // Setup global variables
+    window.Omnigage.terminal.ready(function() {
+      Omnigage = window.Omnigage;
+      oTerminal = Omnigage.terminal;
+    });
+  });
+
+  // show buttons
+  $('#terminal-show-dialer').on('click', function (e) {
+    e.preventDefault();
+    oTerminal.show('dialer');
+  });
+  $('#terminal-show-texter').on('click', function (e) {
+    e.preventDefault();
+    oTerminal.show('texter');
+  });
+  $('#terminal-show-emailer').on('click', function (e) {
+    e.preventDefault();
+    oTerminal.show('emailer');
+  });
+
+  // input buttons
+  $('#form-dialer').on('submit', function (e) {
+    e.preventDefault();
+    var inputTo = $(this).find('.inputTo').val();
+    var inputFrom = $(this).find('.inputFrom').val();
+    var inputParentTo = $(this).find('.inputParentTo').val();
+    var inputVoiceTemplate = $(this).find('.inputVoiceTemplate').val();
+    oTerminal.inputs({
+      to: inputTo,
+      from: inputFrom,
+      parentTo: inputParentTo,
+      voiceTemplate: inputVoiceTemplate,
+    });
+  });
+
+});
